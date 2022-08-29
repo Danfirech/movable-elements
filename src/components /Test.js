054613d30d2205b80ee6e72d91193139f4d20cb7
Dan Possehl <danielbpossehl@gmail.com>
2022-08-28 20:26:39 -0700

    pics in

--- src/components /Test.js
+++ src/components /Test.js
@@ -2,57 +2,146 @@ import React from "react";
 import styled from "styled-components";
 import Draggable from "react-draggable"; // The default
 import Pic1 from "../Media/Pic1.jpg";
+import Pic2 from "../Media/Pic2.jpg";
+import Pic3 from "../Media/Pic3.jpg";
+import Pic4 from "../Media/Pic4.jpg";
+import Pic5 from "../Media/Pic5.jpg";
+import Pic6 from "../Media/Pic6.jpg";
+import Pic7 from "../Media/Pic7.jpg";
+import Pic8 from "../Media/Pic8.jpg";
+import Pic9 from "../Media/Pic9.jpg";
+import Pic10 from "../Media/Pic10.jpg";
 
 const Container = styled.div`
   height: 3000px;
   width: 100%;
   background-color: white;
   overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+`;
+
+const Testy = styled.div`
+  height: 30%;
+  width: 100%;
 `;
 
 const Picture1 = styled.div`
-  height: 200px;
-  width: 350px;
-  position: fixed;
+  height: 400px;
+  width: 750px;
   background-image: url(${Pic1});
   background-size: cover;
   transform: rotate(20deg);
-
-  .transform {
-    transform: rotate(20deg);
-    margin-top: 700px;
-    margin-right: 400px;
-  }
+  position: absolute;
+  z-index: 1;
+  margin-left: 400px;
+  margin-top: 900px;
 `;
 
-const MovableBox2 = styled.div`
-  height: 100px;
-  width: 200px;
-  background-color: purple;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Picture2 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic2});
+  background-size: cover;
+  transform: rotate(-4deg);
+  position: absolute;
+  z-index: 2;
+  margin-left: 1700px;
+  margin-top: 2000px;
 `;
 
-const MovableBox3 = styled.div`
-  height: 140px;
-  width: 130px;
-  background-color: yellow;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Picture3 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic3});
+  background-size: cover;
+  transform: rotate(0deg);
+  position: absolute;
+  margin-left: -100px;
+  margin-top: 1200px;
+  z-index: 3;
 `;
 
-const MovableBox4 = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: red;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const Picture4 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic4});
+  background-size: cover;
+  transform: rotate(-10deg);
+  position: absolute;
+  margin-left: 400px;
+  margin-top: 100px;
+  z-index: 4;
+`;
+
+const Picture5 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic5});
+  background-size: cover;
+  transform: rotate(-10deg);
+  position: absolute;
+  margin-left: 900px;
+  margin-top: 100px;
+  z-index: 4;
+`;
+
+const Picture6 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic6});
+  background-size: cover;
+  transform: rotate(-10deg);
+  position: absolute;
+  margin-left: 1400px;
+  margin-top: 1100px;
+  z-index: 4;
+`;
+
+const Picture7 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic7});
+  background-size: cover;
+  transform: rotate(-10deg);
+  position: absolute;
+  margin-left: 1000px;
+  margin-top: 800px;
+  z-index: 4;
+`;
+
+const Picture8 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic8});
+  background-size: cover;
+  transform: rotate(10deg);
+  position: absolute;
+  margin-left: 700px;
+  margin-top: 2600px;
+  z-index: 4;
+`;
+
+const Picture9 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic9});
+  background-size: cover;
+  transform: rotate(30deg);
+  position: absolute;
+  margin-left: 1400px;
+  margin-top: 1500px;
+  z-index: 4;
+`;
+
+const Picture10 = styled.div`
+  height: 400px;
+  width: 750px;
+  background-image: url(${Pic10});
+  background-size: cover;
+  transform: rotate(-10deg);
+  position: absolute;
+  margin-left: 400px;
+  margin-top: 2100px;
+  z-index: 4;
 `;
 
 const Test = () => {
@@ -60,24 +149,54 @@ const Test = () => {
     <div>
       <Container>
         <Draggable>
-          <div className="transform">
+          <div>
             <Picture1 />
           </div>
         </Draggable>
         <Draggable>
-          <MovableBox2>
-            <h5>Move Me Spencer!</h5>
-          </MovableBox2>
+          <div>
+            <Picture2 />
+          </div>
         </Draggable>
         <Draggable>
-          <MovableBox3>
-            <h5>Move Me Spencer!</h5>
-          </MovableBox3>
+          <div>
+            <Picture3 />
+          </div>
         </Draggable>
         <Draggable>
-          <MovableBox4>
-            <h5>Move Me Spencer!</h5>
-          </MovableBox4>
+          <div>
+            <Picture4 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture5 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture6 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture7 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture8 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture9 />
+          </div>
+        </Draggable>
+        <Draggable>
+          <div>
+            <Picture10 />
+          </div>
         </Draggable>
       </Container>
     </div>
